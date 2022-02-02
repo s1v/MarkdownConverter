6b065fd16e32c518274214ddfd38b98992d480ba
kazuki yanagisawa <thank_area@yahoo.co.jp>
2022-02-03 00:59:05 +0900

    DropZone Created

--- MarkdownConverter/Program.cs
+++ MarkdownConverter/Program.cs
@@ -1,7 +1,6 @@
 using MarkdownConverter;
 using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
